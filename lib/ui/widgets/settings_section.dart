c5901ab3fdc4e032426fd15c732e44d920e5c72f
THR3ATB3AR <threatbevr@gmail.com>
2025-03-12 21:26:15 +0100

    made widget for settings sections

---
+++ lib/ui/widgets/settings_section.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class SettingsSection extends StatelessWidget {
+  const SettingsSection({super.key, required this.title, required this.content});
+  final String title;
+  final Widget content;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom:8.0),
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: const BorderRadius.all(Radius.circular(10)),
+          color: Colors.black.withOpacity(0.2),
+        ),
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  title,
+                  style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: content,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
